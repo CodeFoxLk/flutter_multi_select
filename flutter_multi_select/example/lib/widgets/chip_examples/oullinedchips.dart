e476b19b986d24e00b604598997afc06a2a1263a
codefoxlk <eshannimeshadesigns@gmail.com>
2021-11-30 16:32:55 +0530

    package name updated

--- flutter_multi_select/example/lib/widgets/chip_examples/oullinedchips.dart
+++ flutter_multi_select/example/lib/widgets/chip_examples/oullinedchips.dart
@@ -1,6 +1,6 @@
 import 'package:example/widgets/chip_examples/chip_data_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_multi_select/flutter_multi_select.dart';
+import 'package:flutter_multi_selects/flutter_multi_select.dart';
 
 class OutlinedChipsExample extends StatelessWidget {
   const OutlinedChipsExample({
--- flutter_multi_select/example/lib/widgets/multiselect_features/minimumsetup_example.dart
+++ flutter_multi_select/example/lib/widgets/multiselect_features/minimumsetup_example.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_multi_select/flutter_multi_select.dart';
+import 'package:flutter_multi_selects/flutter_multi_select.dart';
 
 class MinimumSetupExample extends StatelessWidget {
   const MinimumSetupExample({
